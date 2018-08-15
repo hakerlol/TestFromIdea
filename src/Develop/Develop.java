7760913856f45ed069ced4227106aae2ccbd9b2d
hakerlol <haker-1996@mail.ru>
2018-08-15 23:22:02 +0700

    Initial commit

---
+++ src/Develop/Develop.java
@@ -0,0 +1,16 @@
+package Develop;
+
+public class Develop {
+    public String string;
+    public Develop(String string) {
+        System.out.println(string);
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
